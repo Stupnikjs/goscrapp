d1e810f716d4371567c0b64fdab9681bf59ac9e6
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-05-04 07:54:59 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -3,7 +3,13 @@ package main
 
 func GetMonthMap() map[string]int {
 
+    mois := []string{"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"}
+   monthIndexMap := make(map[string]int)
 
+    // Populate the map with French month names and their respective index values
+    for i, moisItem := range mois {
+        monthIndexMap[moisItem] = i
+    }
 
 
 
