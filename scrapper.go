85a557ed7b3054abe99e514dfb8179c636e07c71
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-05-04 07:56:25 +0200

    Update scrapper.go

--- scrapper.go
+++ scrapper.go
@@ -24,14 +24,9 @@ func getDateInfo(node *cdp.Node) (time.Time,error) {
 
                         if node.Parent.NodeName == "STRONG" {
                                 monthStr := node.NodeValue
-    // process 
-    mois := []string{"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"}
-   monthIndexMap := make(map[string]int)
-
-    // Populate the map with French month names and their respective index values
-    for i, moisItem := range mois {
-        monthIndexMap[moisItem] = i
-    }
+   
+    
+    monthIndexMap := GetMonthMap()
     mounth = monthIndexMap[monthStr]
 
 }
