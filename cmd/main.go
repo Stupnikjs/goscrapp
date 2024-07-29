86d51f8b3599b14f9314a50710423d0a205ee1d4
stupnikk <andre.lacassagne82@gmail.com>
2024-07-29 20:29:20 +0200

    ok

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
@@ -12,37 +11,55 @@ import (
 )
 
 func main() {
-	start := time.Now()
-	var nodes []*cdp.Node
-	var selector string = `//div[@class="col-md-6 clickable visible-lg visible-md"]//*`
-	var url string = "https://protiming.fr/Runnings/liste"
 
+	var nodes []*cdp.Node
+	var selector string = `//ul[@class="tablelike"]//a/@href`
+	var url string = "https://www.lemoniteurdespharmacies.fr/emploi/espace-candidats/lire-les-annonces.html"
+
+	// recuperer le nombres de pages en scrappant
+	for i := range [17]int{} {
+
+		urls := Scrap(selector, url, nodes, i)
+		fmt.Println(urls)
+
+	}
+
+}
+
+func Scrap(selector string, URL string, nodes []*cdp.Node, i int) *[]string {
 	ctx, _ := chromedp.NewContext(context.Background())
 	ctx, cancel := context.WithTimeout(ctx, time.Minute*3)
 	defer cancel()
+	var urls = []string{}
+	var url string = ""
+	if i == 0 {
+		url = "https://www.lemoniteurdespharmacies.fr/emploi/espace-candidats/lire-les-annonces.html"
+	}
+	url = fmt.Sprintf("https://www.lemoniteurdespharmacies.fr/emploi/espace-candidats/lire-les-annonces-%d.html", i)
 
-	Scrap(ctx, selector, url, nodes)
-
-	fmt.Println(time.Since(start))
-}
-
-func Scrap(ctx context.Context, selector string, URL string, nodes []*cdp.Node) {
-	obj := map[string]string{}
 	err := chromedp.Run(
 		ctx,
-		chromedp.Navigate(URL),
-		&chromedp.Tasks{
-			chromedp.Nodes(selector, &nodes),
-			chromedp.ActionFunc(func(ctx context.Context) error {
+		chromedp.Navigate(url),
+		chromedp.Nodes(selector, &nodes),
+		chromedp.ActionFunc(func(ctx context.Context) error {
 
-				ProcessNode(os.Stdout, nodes, &obj)
-				return nil
-			}),
-		},
+			ProcessNodes(nodes, &urls)
+			return nil
+		}),
 	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	return &urls
+}
 
+func ProcessNodes(nodes []*cdp.Node, urls *[]string) *[]string {
+
+	for _, node := range nodes {
+		if node.NodeType == cdp.NodeTypeElement {
+			*urls = append(*urls, node.Attributes[1])
+		}
+	}
+	return urls
 }
